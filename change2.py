017ccc270640723e9e766a69a0d55ea6182628a1
brind <bbhattacharya@student.bridgew.edu>
2023-02-09 09:14:15 -0500

    Initial commit

---
+++ change2.py
@@ -0,0 +1,12 @@
+def main():
+    print("Change Counter")
+    print()
+    print("Please enter the count of each coin type.")
+    quarters = int(input("Quarters: "))
+    dimes = int(input("Dimes: "))
+    nickels = int(input("Nickels: "))
+    pennies = int(input("Pennies: "))
+    total = .25*quarters + .10*dimes + .05*nickels + .01*pennies
+    print()
+    print("The total value of your change is", total)
+main()
